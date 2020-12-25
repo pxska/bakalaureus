6dfce710b80d713c606bce206a230654de274adc
pxska <kristjanparnmets@gmail.com>
2020-12-25 19:34:56 +0200

    deleted some files

--- trainer_info/readdata.py
+++
@@ -1,9 +0,0 @@
-with open("data.txt", "r", encoding="UTF-8") as f:
-    txt = f.readlines()
-
-time = []
-for x in txt:
-    if "Total seconds required for training" in x:
-        time.append(float(x.split(":")[1].strip("\n").lstrip()))
-
-print(sum(time))
